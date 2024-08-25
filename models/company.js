f4e08f47cea57b6eb167077ccae11c6e7c722712
Yaswanth <22jn1a05f9@svcn.ac.in>
2024-08-26 00:30:03 +0530

    Update company.js

--- models/company.js
+++ models/company.js
@@ -14,16 +14,19 @@ const companySchema = new mongoose.Schema({
     match: [/.+@.+\..+/, 'Please enter a valid email address']
   },
   password: {
-    type: String,
-    required: true,
-    minlength: [8, 'Password must be at least 8 characters long']
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/^\d{10}$/, 'Mobile number must be 10 digits long']
-  }
-});
 
+    
+        type: String,
+        required: true,
+        minlength: [8, 'Password must be at least 8 characters long']
+      },
+      mobile: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\d{10}$/, 'Mobile number must be 10 digits long']
+      }
+    });
+    
+   
 const company = mongoose.model('Company', companySchema);
