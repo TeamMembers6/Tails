c14684aa79973d6075a436655a17fb8fc44bbe73
Yaswanth <22jn1a05f9@svcn.ac.in>
2024-08-26 00:48:09 +0530

    Update company.js

--- models/company.js
+++ models/company.js
@@ -1,32 +1,12 @@
-
 const mongoose = require('mongoose');
 
-// Define the user schema
+// Define schema
 const companySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Please enter a valid email address']
-  },
-  password: {
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
 
-    
-        type: String,
-        required: true,
-        minlength: [8, 'Password must be at least 8 characters long']
-      },
-      mobile: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^\d{10}$/, 'Mobile number must be 10 digits long']
-      }
-    });
-    
-   
-const company = mongoose.model('Company', companySchema);
+// Define model
+const Company = mongoose.model('Company', companySchema);
+
+// Use `Company` instead of `company`
